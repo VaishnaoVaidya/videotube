37006ff877113565dfdac9792e7a9a91fa92a571
Vaishnao Vaidya <vaishnaovaidya2001@gmail.com>
2026-03-25 18:32:20 +0530

    added root route

--- backend/src/routes/healthcheck.routes.js
+++ backend/src/routes/healthcheck.routes.js
@@ -2,5 +2,10 @@ import { Router } from "express";
 
 const router = Router()
 
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "OK"
+    });
+});
 
 export default router
